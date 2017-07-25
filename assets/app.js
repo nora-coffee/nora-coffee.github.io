40e20d000e6c7d881fa4cc2514380111886b9c81
akiyahoan <k@akiyahoan.com>
2017-07-25 09:04:50 +0900

    ga

--- assets/app.js
+++ assets/app.js
@@ -3,3 +3,11 @@ sr.reveal('.logo', 800);
 sr.reveal('.logo-image',{delay: 500}, 800);
 
 sr.reveal('.headline', 800);
+
+$(document).ready(function(){
+  $(".beans").hover(function(){
+    $(".beans-description").fadeIn();
+  }, function(){
+    $(".beans-description").fadeOut();
+  })
+});
